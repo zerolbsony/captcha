f780d828df51d9cfd0bdc5bed04c0e1aff17c9c2
ssh://5337a7f850044680710007dc@php-iugou.rhcloud.com/~/git/php.git/ <zerolbsony@163.com>
2014-12-23 17:12:41 +0800

    将php_printf的方式改为return，这样函数不是直接打印值，而是返回一个字符串。

--- captcha.c
+++ captcha.c
@@ -30,19 +30,29 @@ PHP_FUNCTION(generate_captcha)
      }
    
      if(s1 == '0'){
-		php_printf(s1);
-	 }else{
-		char s2[10];
-		int j;
-		memset(s2, 0, sizeof(s2));
-		srand((int)time(0));
-		for(i=0; i<amt; i++){
-			j = rand() % Z_STRLEN(dictpath);
-			memcpy(s2, s1+j, 1);
-			php_printf(s2);
-			memset(s2, 0, sizeof(s2));
-		}
-	 }
+	     int len = strlen(s1);
+	     char* ret = (char*)emalloc(len);
+	     memcpy(ret, s1, len);
+	     memset(s1, 0, sizeof(s1));
+	     RETURN_STRINGL(ret, len, 0);	     	
+     }else{
+	     char s2[10];
+	     int j;
+	     int offset = 0;
+	     memset(s2, 0, sizeof(s2));
+	     srand((int)time(0));
+	     for(i=0; i<amt; i++){
+		     j = rand() % Z_STRLEN(dictpath);
+		     memcpy(s2+offset, s1+j, 1);
+		     offset = offset+1;
+		     memset(s2, 0, sizeof(s2));
+	     }
+	     int len = strlen(s2);
+	     char* ret = (char*)emalloc(len);
+	     memcpy(ret, s2, len);
+	     memset(s2, 0, sizeof(s2));
+	     RETURN_STRINGL(ret, len, 0);
+     }
 }
 
 static zend_function_entry php_captcha_functions[] = {
